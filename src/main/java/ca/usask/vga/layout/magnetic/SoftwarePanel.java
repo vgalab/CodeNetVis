e5937756975ab22e9b2f4c43b93dcb657d142c70
Mykyta Shvets <nikitashets28@gmail.com>
2022-08-04 11:59:02 -0600

    Added scrollbars and proper resizing

--- src/main/java/ca/usask/vga/layout/magnetic/SoftwarePanel.java
+++ src/main/java/ca/usask/vga/layout/magnetic/SoftwarePanel.java
@@ -5,11 +5,8 @@ import org.cytoscape.application.swing.CytoPanelName;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Hashtable;
 
 public class SoftwarePanel extends JPanel implements CytoPanelComponent2 {
 
@@ -24,15 +21,18 @@ public class SoftwarePanel extends JPanel implements CytoPanelComponent2 {
         this.layout = layout;
         this.style = style;
 
-        var mainLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
-        setLayout(mainLayout);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        this.setBorder(new EmptyBorder(10, 10, 10, 10));
+        JPanel innerPanel = new JPanel() {
+            public Dimension getPreferredSize() {
+                return new Dimension(getParent().getSize().width, super.getPreferredSize().height);
+            }
+        };
 
         // Temporary DATA panel
         JPanel panel = createTitledPanel("Data");
         panel.add(group(new JLabel("GitHub Link:"), new JTextField("www.example.com")));
-        add(panel);
+        innerPanel.add(panel);
 
 
         // Temporary POLE panel
@@ -42,29 +42,42 @@ public class SoftwarePanel extends JPanel implements CytoPanelComponent2 {
         //panel.add(new JList<>(new String[]{"PrintStream", "PrintMode", "PrintFiles"}));
         panel.add(group(new JButton("Add poles"), new JButton("Remove poles")));
         panel.add(group(new JLabel("Poles:"), new JLabel("String, Object...")));
-        add(panel);
-
+        innerPanel.add(panel);
 
         // Temporary FILTERING panel
         panel = createTitledPanel("Filtering");
         panel.add(group(new JRadioButton("No dependencies")));  // NOTE: Need button group
         panel.add(group(new JRadioButton("Unique dependencies")));
-        add(panel);
+        innerPanel.add(panel);
 
 
         // LAYOUT panel
-        add(createLayoutPanel());
+        innerPanel.add(createLayoutPanel());
 
 
         // STYLE panel
-        add(createStylePanel());
+        innerPanel.add(createStylePanel());
+
+
+        // Scroll Pane
+        innerPanel.setLayout(new BoxLayout(innerPanel, BoxLayout.Y_AXIS));
+        innerPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        var scrollPane = new JScrollPane(innerPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setBorder(null);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+
+        add(scrollPane);
     }
 
     protected JPanel createTitledPanel(String title) {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.add(group(new JLabel(bold(title))));
-        panel.setBorder(BorderFactory.createLineBorder(Color.lightGray));
+        //panel.setBorder(BorderFactory.createLineBorder(Color.lightGray));
+        panel.setBorder(BorderFactory.createCompoundBorder(new EmptyBorder(0,0,10,0),
+                BorderFactory.createLineBorder(Color.lightGray)));
         return panel;
     }
 
@@ -73,21 +86,16 @@ public class SoftwarePanel extends JPanel implements CytoPanelComponent2 {
 
         var radiusEditor = new JSlider();
 
-        int MAX = 10000;
+        int MAX = 100;
 
         radiusEditor.setMinimum(0);
         radiusEditor.setMaximum(MAX);
         radiusEditor.setValue(MAX/4);
         radiusEditor.setPaintTicks(true);
-        radiusEditor.setPaintLabels(true);
         radiusEditor.setMajorTickSpacing(MAX/4);
         radiusEditor.setMinorTickSpacing(MAX/20);
 
-        /*var labelTable = new Hashtable<Integer, JLabel>();
-        labelTable.put(0, new JLabel("0") );
-        radiusEditor.setLabelTable( labelTable );*/
-
-        radiusEditor.addChangeListener(e -> layout.setPinRadius(radiusEditor.getValue()));
+        radiusEditor.addChangeListener(e -> layout.setPinRadius(radiusEditor.getValue()*100));
         layout.setPinRadius(radiusEditor.getValue());
 
         panel.add(label("Circle radius: " + MAX/4, radiusEditor));
@@ -112,10 +120,7 @@ public class SoftwarePanel extends JPanel implements CytoPanelComponent2 {
         // CONTENTS
         panel.add(group(new JLabel("Node size based on"), new JComboBox<String>(new String[]{"Fixed"/*, "Indegree", "Outdegree"*/})));
 
-        var sizeEditor = new JSlider();
-        sizeEditor.setValue(50);
-        sizeEditor.setMinimum(0);
-        sizeEditor.setMaximum(100);
+        var sizeEditor = new JSlider(0, 100, 50);
         sizeEditor.setPaintTicks(true);
         sizeEditor.setMajorTickSpacing(50);
         sizeEditor.setMinorTickSpacing(10);
@@ -124,10 +129,7 @@ public class SoftwarePanel extends JPanel implements CytoPanelComponent2 {
 
         panel.add(label("Node size: 50", sizeEditor));
 
-        var transparencyEditor = new JSlider();
-        transparencyEditor.setValue(100);
-        transparencyEditor.setMinimum(0);
-        transparencyEditor.setMaximum(255);
+        var transparencyEditor = new JSlider(0, 255, 100);
         transparencyEditor.setPaintTicks(true);
         transparencyEditor.setMajorTickSpacing(50);
         transparencyEditor.setMinorTickSpacing(10);
@@ -157,6 +159,8 @@ public class SoftwarePanel extends JPanel implements CytoPanelComponent2 {
     private JPanel label(String text, JSlider component) {
         var label = new JLabel(text);
         component.addChangeListener(e -> updateLabelValue(label, component.getValue()));
+        if (component.getPaintLabels())
+            return group(50, label, component);
         return group(label, component);
     }
 
@@ -169,11 +173,15 @@ public class SoftwarePanel extends JPanel implements CytoPanelComponent2 {
     }
 
     private JPanel group(JComponent... components) {
+        return group(35, components);
+    }
+
+    private JPanel group(int height, JComponent... components) {
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(1, components.length, 10, 10));
-        //panel.setBackground(Color.yellow);
         panel.setBorder(new EmptyBorder(5, 5,5,5));
-        panel.setMaximumSize(new Dimension(10000, 35));
+        panel.setMaximumSize(new Dimension(10000, height));
+        panel.setPreferredSize(new Dimension(panel.getPreferredSize().width, height));
         for (JComponent c : components) {
             panel.add(c);
         }
